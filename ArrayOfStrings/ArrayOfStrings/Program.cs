dbc60a825911190511c0b4d492fefee759ac839f
Kazeem <97337763+thedoskit@users.noreply.github.com>
2024-05-12 21:59:06 +0100

    commit
    new

---
+++ ArrayOfStrings/ArrayOfStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] arrayofstrings = { "Volvo", "BMW", "Ford", "Mazda", "Esther", "Qosim", "Doskit", "Dosunmu", "Kolawole", "Kazeem", "Idowu", "Temitayo" };
+
+arrayofstrings[3] = "Taiwo";
+arrayofstrings[0] = "Lexus";
+arrayofstrings[1] = "Toyota";
+
+//for (int i = 0; i < arrayofstrings.Length; i++)
+foreach (string s in arrayofstrings)
+{
+    char lastLetter = s[s.Length - 1];
+
+    char lastLetterToUpper = char.ToUpper(lastLetter);
+
+    //arrayofstrings = lastLetterToUpper.ToString();
+
+    Console.WriteLine(s.Replace(lastLetter, lastLetterToUpper));
+
+}
+
+
+
+
